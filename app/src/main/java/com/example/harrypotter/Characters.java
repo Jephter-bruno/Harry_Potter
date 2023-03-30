ee27e4747bdbf0f7684fc38badec5e06b17cdb44
Jephter-bruno <jephter35679577@gmail.com>
2023-03-31 00:42:56 +0300

    Replaced Retrofit with Volley

--- app/src/main/java/com/example/harrypotter/Characters.java
+++ app/src/main/java/com/example/harrypotter/Characters.java
@@ -1,139 +1,30 @@
 package com.example.harrypotter;
 
-import com.google.gson.annotations.SerializedName;
-
 public class Characters {
-     @SerializedName("name")
-    String name;
-    @SerializedName("species")
-     String species;
-    @SerializedName("gender")
-     String gender;
-    @SerializedName("house")
-     String house;
-    @SerializedName("alternate_names")
-     String alternate_name;
-    @SerializedName("id")
-    int id;
-    @SerializedName("dateOfBirth")
-     String dateOfBirth;
-    @SerializedName("wizard")
-     String wizard;
-    @SerializedName("ancestry")
-     String ancestry;
-    @SerializedName("eyeColour")
-     String eyeColour;
-    @SerializedName("hairColour")
-     String hairColour;
-    @SerializedName("wand")
-     String wand;
-    @SerializedName("actor")
-     String core;
-    @SerializedName("alive")
-     String wood;
-    @SerializedName("image")
-     String image;
 
+    private String name , image , dateOfBirth;
+    private String gender;
 
-    // Add more fields as necessary
-
-    public Characters(String name, String species, String gender, String house, String alternate_name, int id, String dateOfBirth, String wizard, String ancestry, String eyeColour, String hairColour, String wand, String core, String wood, String image) {
+    public Characters(String name, String image, String dateOfBirth, String gender) {
         this.name = name;
-        this.species = species;
-        this.gender = gender;
-        this.house = house;
-        this.alternate_name = alternate_name;
-        this.id = id;
-        this.dateOfBirth = dateOfBirth;
-        this.wizard = wizard;
-        this.ancestry = ancestry;
-        this.eyeColour = eyeColour;
-        this.hairColour = hairColour;
-        this.wand = wand;
-        this.core = core;
-        this.wood = wood;
         this.image = image;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
     }
-
-
-    // Constructor and getter/setter methods
 
     public String getName() {
         return name;
     }
 
-
-
-    public String getSpecies() {
-        return species;
+    public String getImage() {
+        return image;
     }
-
-
-
-    public String getGender() {
-        return gender;
-    }
-
-
-
-    public String getHouse() {
-        return house;
-    }
-
-
-    public String getAlternate_name() {
-        return alternate_name;
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
 
     public String getDateOfBirth() {
         return dateOfBirth;
     }
 
-
-    public String getWizard() {
-        return wizard;
+    public String getGender() {
+        return gender;
     }
-
-
-    public String getAncestry() {
-        return ancestry;
-    }
-
-
-    public String getEyeColour() {
-        return eyeColour;
-    }
-
-
-    public String getHairColour() {
-        return hairColour;
-    }
-
-
-    public String getWand() {
-        return wand;
-    }
-
-
-    public String getCore() {
-        return core;
-    }
-
-
-    public String getWood() {
-        return wood;
-    }
-
-
-    public String getImage() {
-        return image;
-    }
-
 }
-
---
+++ app/src/main/java/com/example/harrypotter/MovieAdapter.java
@@ -0,0 +1,86 @@
+package com.example.harrypotter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder> {
+
+    private Context context;
+    private List<Characters> characterlist;
+
+    public MovieAdapter(Context context , List<Characters> characters){
+        this.context = context;
+        characterlist = characters;
+    }
+    @NonNull
+    @Override
+    public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.character_item , parent , false);
+        return new MovieHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+
+        Characters character = characterlist.get(position);
+        holder.gender.setText(character.getGender());
+        holder.name.setText(character.getName());
+        holder.dateofBirth.setText(character.getDateOfBirth());
+        Glide.with(context).load(character.getImage()).into(holder.image);
+
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context , DetailActivity.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("name" , character.getName());
+                bundle.putString("birth" , character.getDateOfBirth());
+                bundle.putString("image" , character.getImage());
+                bundle.putString("gender" , character.getGender());
+
+                intent.putExtras(bundle);
+
+                context.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return characterlist.size();
+    }
+
+    public class MovieHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView name , gender , dateofBirth;
+        LinearLayout linearLayout;
+
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.image);
+            name = itemView.findViewById(R.id.name);
+            gender = itemView.findViewById(R.id.gender);
+            dateofBirth = itemView.findViewById(R.id.dateofbirth);
+            linearLayout = itemView.findViewById(R.id.main_layout);
+        }
+    }
+}
