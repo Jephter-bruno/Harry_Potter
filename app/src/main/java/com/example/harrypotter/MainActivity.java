1fccafcf92c4d9ef7a10739da4a0bfad33f8195a
Jephter-bruno <jephter35679577@gmail.com>
2023-04-02 22:36:27 +0300

    Added CharacterSearchAsyncTask

--- app/src/main/java/com/example/harrypotter/MainActivity.java
+++ app/src/main/java/com/example/harrypotter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.harrypotter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,7 +10,6 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 
@@ -26,12 +26,17 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RequestQueue requestQueue;
     private List<Characters> charactersList;
-    private EditText mSearchEditText;
+    private SearchView mSearchEditText;
     private Button button;
     private RecyclerView mRecyclerView;
     private CharacterAdapter mBookAdapter;
@@ -50,69 +55,81 @@ public class MainActivity extends AppCompatActivity {
         requestQueue = VolleySingleton.getmInstance(this).getRequestQueue();
 
         charactersList = new ArrayList<>();
-            fetchCharacters();
+        fetchCharacters();
         mSearchEditText = findViewById(R.id.searchView);
+        String query = (String) mSearchEditText.getQuery();
 
-      button.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              String query = mSearchEditText.getText().toString();
-              CharacterSearchAsyncTask task = new CharacterSearchAsyncTask(MainActivity.this, mRecyclerView, mBookAdapter);
-              task.execute(query);
-          }
-      });
-
-
-
-
-
-}
-
-    private void fetchCharacters() {
-
-        String url = "https://hp-api.onrender.com/api/characters";
-
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(JSONArray response) {
+            public void onClick(View view) {
+                Call<List<Characters>> call = CharacterApiService.searchCharacters(query);
+                call.enqueue(new Callback<List<Characters>>() {
 
-                for (int i = 0 ; i < response.length() ; i ++){
-                    try {
-                        JSONObject jsonObject = response.getJSONObject(i);
-                        String name = jsonObject.getString("name");
-                        String gender = jsonObject.getString("gender");
-                        String image = jsonObject.getString("image");
-                        String date = jsonObject.getString("dateOfBirth");
-                        String house = jsonObject.getString("house");
-
-                        String species = jsonObject.getString("species");
-                        String year = jsonObject.getString("yearOfBirth");
-                        String wizard = jsonObject.getString("wizard");
-                        String eyecolour = jsonObject.getString("eyeColour");
-                        String haircolour = jsonObject.getString("hairColour");
-                        String patronus = jsonObject.getString("patronus");
-                        String actor = jsonObject.getString("actor");
-                        String alive = jsonObject.getString("alive");
-
-                        Characters characterrs = new Characters(name , image ,
-                                gender , date,house, species, year, wizard, eyecolour,haircolour,patronus,actor, alive);
-                        charactersList.add(characterrs);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                    @Override
+                    public void onResponse(Call<List<Characters>> call, retrofit2.Response<List<Characters>> response) {
+                        if (response.isSuccessful()) {
+                            List<Characters> characters = response.body();
+                            characters.clear();
+                            characters.addAll(response.body());
+                            mBookAdapter.notifyDataSetChanged();
+                        }
                     }
 
-                    CharacterAdapter adapter = new CharacterAdapter(MainActivity.this , charactersList);
+                    @Override
+                    public void onFailure(Call<List<Characters>> call, Throwable t) {
 
-                    recyclerView.setAdapter(adapter);
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
         });
+            }
 
-        requestQueue.add(jsonArrayRequest);
-    }
-}
+            private void fetchCharacters() {
+
+                String url = "https://hp-api.onrender.com/api/characters";
+
+                JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                String name = jsonObject.getString("name");
+                                String gender = jsonObject.getString("gender");
+                                String image = jsonObject.getString("image");
+                                String date = jsonObject.getString("dateOfBirth");
+                                String house = jsonObject.getString("house");
+
+                                String species = jsonObject.getString("species");
+                                String year = jsonObject.getString("yearOfBirth");
+                                String wizard = jsonObject.getString("wizard");
+                                String eyecolour = jsonObject.getString("eyeColour");
+                                String haircolour = jsonObject.getString("hairColour");
+                                String patronus = jsonObject.getString("patronus");
+                                String actor = jsonObject.getString("actor");
+                                String alive = jsonObject.getString("alive");
+
+                                Characters characterrs = new Characters(name, image,
+                                        gender, date, house, species, year, wizard, eyecolour, haircolour, patronus, actor, alive);
+                                charactersList.add(characterrs);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            CharacterAdapter adapter = new CharacterAdapter(MainActivity.this, charactersList);
+
+                            recyclerView.setAdapter(adapter);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                requestQueue.add(jsonArrayRequest);
+            }
+        }
---
+++ app/src/main/java/com/example/harrypotter/Search/CharacterApiService.java
@@ -0,0 +1,4 @@
+package com.example.harrypotter.Search;
+
+public class CharacterApiService {
+}
